41987a7b92aba473283e872218cbeca4919e8724
Jerpen80 <dra_penny@yahoo.com>
2021-06-26 23:32:50 +0200

    Added difficulty system

--- mathpuzzle.py
+++ mathpuzzle.py
@@ -3,24 +3,51 @@ play = 'y'
 counter = 0
 won = 0
 lost = 0
-def getallen():
+def easy():
     x = random.randint(-10,10)
     y = random.randint(-10,10)
     return x, y
-
-print("\nWelcome to my puzzle!\n")
-print("Find out what x and y are!")
-
+def medium():
+    x = random.randint(-20,20)
+    y = random.randint(-20,20)
+    return x, y
+def hard():
+    x = random.randint(-50,50)
+    y = random.randint(-50,50)
+    return x, y
+print("\nWelcome to my puzzle!")
 while play == 'y':
-    x, y = getallen()
+    try:
+        print("\nDo you want to play easy, medium or hard?")
+        level = int(input("1 = easy, 2 = medium, 3 = hard. Typ 1, 2 or 3 and press enter: "))
+        while level < 1: 
+            print("Please enter a number from 1 to 3")
+            level = int(input("1 = easy, 2 = medium, 3 = hard. Typ 1, 2 or 3 and press enter: "))
+            while level > 3:
+                print("Please enter a number from 1 to 3")
+                level = int(input("1 = easy, 2 = medium, 3 = hard. Typ 1, 2 or 3 and press enter: "))
+        while level > 3:
+            print("Please enter a number from 1 to 3")
+            level = int(input("1 = easy, 2 = medium, 3 = hard. Typ 1, 2 or 3 and press enter: "))
+            while level < 1: 
+                print("Please enter a number from 1 to 3")
+                level = int(input("1 = easy, 2 = medium, 3 = hard. Typ 1, 2 or 3 and press enter: "))
+    except ValueError:
+        print("Please enter a number from 1 to 3")
+        continue
+    print("Find out what x and y are!")
+    if level == 1:
+        x, y = easy()
+    elif level == 2:
+        x, y = medium()
+    else:
+        x, y = hard()
     counter += 1
     a = x + y
     b = x * y
     print("\nx + y = "+ str(a))
     print("\nx * y = "+ str(b))
-    print()
-    
-    e = int(input("x = "))
+    e = int(input("\nx = "))
     f = int(input("y = "))
     c = e + f
     d = e * f
@@ -28,7 +55,6 @@ while play == 'y':
         if b == d:
             won += 1
             print("\nWell done!")
-
         else:
             lost +=1
             print("\nWrong answer.. You can do better than that! Solution: x was "+ str(x) + " and y was "+ str(y))
@@ -36,7 +62,6 @@ while play == 'y':
         lost +=1
         print("\nWrong answer.. You can do better than that! Solution: x was "+ str(x) + " and y was "+ str(y))   
     play = input("\nWant to try again? Type y and press enter to play again: ")
-
 print("\nYou've played "+ str(counter) +" games in total. You've won "+ str(won) + " game(s) and lost "+ str(lost) +" game(s)!")
 print("\nThank you for playing! Have a nice day!")
 
